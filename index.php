9bde9daf4b22690c035847c004d8ed6dbb8e4f01
VLuca997 <kulertoast997@gmail.com>
2023-08-22 16:13:22 +0200

    ADD Array

--- index.php
+++ index.php
@@ -21,9 +21,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/framework.css">
-    <link rel="stylesheet" href="css/general.css">
-    <link rel="stylesheet" href="css/mediaquery.css">
     <link rel="stylesheet" href="css/style.css">
 
     
@@ -31,26 +28,51 @@
 <body id="debug">
 
 
+    <?php
+        //ARRAY 
+        
+        $hotels = [
+    
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+    
+        ];
+    ?>
 
 
-    <!-- HEADER -->
-    <header>
-        HEADER
-    </header>
-
-
-
-    <!-- MAIN -->
-    <main>
-        MAIN
-    </main>
-
-
-
-    <!-- FOOTER -->
-    <footer>
-        FOOTER
-    </footer>
 
 
 
